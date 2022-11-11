69906416c25ca0c09ec38aa97d51efe28f5c9003
VISS7289 <1526716943@qq.com>
2022-11-11 17:31:25 +0800

    简单GUI练习

--- projects/bootpack.c
+++ projects/bootpack.c
@@ -1,10 +1,110 @@
-//bootpack.c
+// bootpack.c
 void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000 0 // 0黑
+#define COL8_FF0000 1 // 1亮红
+#define COL8_00FF00 2  // 2亮绿
+#define COL8_FFFF00 3  // 3亮黄
+#define COL8_0000FF 4  // 4亮蓝
+#define COL8_FF00FF 5  // 5亮紫
+#define COL8_00FFFF 6  // 6浅亮蓝
+#define COL8_FFFFFF 7  // 7白
+#define COL8_C6C6C6 8  // 8亮灰
+#define COL8_840000 9  // 9暗红
+#define COL8_008400 10 // 10暗绿
+#define COL8_848400 11 // 11暗黄
+#define COL8_000084 12 // 12暗青
+#define COL8_840084 13 // 13暗紫
+#define COL8_008484 14 // 14浅暗蓝
+#define COL8_848484 15 // 15暗灰
 
 void HariMain(void)
 {
-	fin:
-	    io_hlt();
-	    //自建的hlt函数，源目标程序在naskfunc.nas中，c语言本身没有hlt这个命令
-	    goto fin;
+	char *vram;
+	init_palette();		 // 设置调色盘
+	vram = (char *)0xa0000; // 指明地址
+	int xsize = 320, ysize = 200;
+	// 背景
+	boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29); // 整体背景
+	// 导航栏
+	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28); // 灰色margin
+	boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27); // 白色margin
+	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);  //  底部导航栏
+	// 按钮
+	boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24); // 白色margin上
+	boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4); // 白色margin左
+	boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4); // 灰色margin下
+	boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5); // 灰色margin右
+	boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3); // 黑色margin下
+	boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3); // 黑色margin右
+	// 框
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24); // 灰色padding上
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4); // 灰色padding左
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3); // 白色padding下
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3); // 白色padding右
+
+	for (;;)
+	{
+		io_hlt();
+	}
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00, // 0黑
+		0xff, 0x00, 0x00, // 1亮红
+		0x00, 0xff, 0x00, // 2亮绿
+		0xff, 0xff, 0x00, // 3亮黄
+		0x00, 0x00, 0xff, // 4亮蓝
+		0xff, 0x00, 0xff, // 5亮紫
+		0x00, 0xff, 0xff, // 6浅亮蓝
+		0xff, 0xff, 0xff, // 7白
+		0xc6, 0xc6, 0xc6, // 8亮灰
+		0x84, 0x00, 0x00, // 9暗红
+		0x00, 0x84, 0x00, // 10暗绿
+		0x84, 0x84, 0x00, // 11暗黄
+		0x00, 0x00, 0x84, // 12暗青
+		0x84, 0x00, 0x84, // 13暗紫
+		0x00, 0x84, 0x84, // 14浅暗蓝
+		0x84, 0x84, 0x84  // 15暗灰
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags(); // 记录中断许可标志的值
+	io_cli();				   // 将中断许可标志设为0，禁止中断
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++)
+	{
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags); //复原中断标志
+	return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++)
+	{
+		for (x = x0; x <= x1; x++)
+			vram[y * xsize + x] = c;
+	}
+	return;
 }
--- projects/naskfunc.nas
+++ projects/naskfunc.nas
@@ -3,16 +3,78 @@
 ; TAB = 4
 
 [FORMAT "WCOFF"]    ;制作的目标文件的模式,因为要与c文件对接，因此需要生成一个中间的文件
+[INSTRSET "i486p"]  ;该程序是在cpu 486上运行的
 [BITS 32]           ;制作32位的机器语言模式
 
 ;制作目标文件的信息
-[FILE "naskfunc.nas"]   ;源文件名信息
+[FILE "naskfunc.nas"]   ;源文件名信息   ;程序中包含的函数名，一定要以_开头，为了衔接c语言的函数库
          
-         GLOBAL     _io_hlt     ;程序中包含的函数名，一定要以_开头，为了衔接c语言的函数库
+		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL	_io_in8,  _io_in16,  _io_in32
+		GLOBAL	_io_out8, _io_out16, _io_out32
+		GLOBAL	_io_load_eflags, _io_store_eflags
 
-; 以下是实际的函数内容
-[SECTION .text]          ;目标文件中写了这些之后再写程序
+[SECTION .text]
 
-_io_hlt:   ;void io_hlt(void);
-        HLT 
-        RET          
+_io_hlt:	; void io_hlt(void);
+		HLT
+		RET
+
+_io_cli:	; void io_cli(void);
+		CLI
+		RET
+
+_io_sti:	; void io_sti(void);
+		STI
+		RET
+
+_io_stihlt:	; void io_stihlt(void);
+		STI
+		HLT
+		RET
+
+_io_in8:	; int io_in8(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+_io_in16:	; int io_in16(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+_io_in32:	; int io_in32(int port);
+		MOV		EDX,[ESP+4]		; port
+		IN		EAX,DX
+		RET
+
+_io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+_io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+_io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+_io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS
+		POP		EAX
+		RET
+
+_io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; POP EFLAGS
+		RET
