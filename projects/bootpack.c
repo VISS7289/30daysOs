d54b09d3c8c85bdbbc9efa7a85633a2ba7cabd42
VISS7289 <1526716943@qq.com>
2022-11-12 20:20:20 +0800

    显示文字与鼠标

--- projects/bootpack.c
+++ projects/bootpack.c
@@ -1,4 +1,5 @@
 // bootpack.c
+#include <stdio.h>
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -8,9 +9,13 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void putfont8(unsigned char *vram, int xsize, int x, int y, char c, char *font);
+void initScr(unsigned char *vram, int xsize, int ysize);
+void putfont8_asc(unsigned char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
 
-#define COL8_000000 0 // 0黑
-#define COL8_FF0000 1 // 1亮红
+#define COL8_000000 0  // 0黑
+#define COL8_FF0000 1  // 1亮红
 #define COL8_00FF00 2  // 2亮绿
 #define COL8_FFFF00 3  // 3亮黄
 #define COL8_0000FF 4  // 4亮蓝
@@ -26,12 +31,37 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 #define COL8_008484 14 // 14浅暗蓝
 #define COL8_848484 15 // 15暗灰
 
+struct BOOTINFO
+{
+	char cyls, leds, vmode, reserve;
+	short scrnx, scrny;
+	char *vram;
+};
+
 void HariMain(void)
 {
-	char *vram;
-	init_palette();		 // 设置调色盘
-	vram = (char *)0xa0000; // 指明地址
-	int xsize = 320, ysize = 200;
+	struct BOOTINFO *bootinfo = (struct BOOTINFO *)0x0ff0;
+	char s[40], mcursor[256];
+	int mx, my;
+	mx = (bootinfo->scrnx - 16) / 2;
+	my = (bootinfo->scrny - 28 - 16) / 2;
+	
+	init_palette(); // 设置调色盘
+	initScr(bootinfo->vram, bootinfo->scrnx, bootinfo->scrny);
+
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(bootinfo->vram, bootinfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+	sprintf(s, "(%d,%d)", mx, my);
+	putfont8_asc(bootinfo->vram, bootinfo->scrnx, 16, 20, COL8_FFFFFF, s);
+	for (;;)
+	{
+		io_hlt();
+	}
+}
+
+void initScr(unsigned char *vram, int xsize, int ysize)
+{
 	// 背景
 	boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29); // 整体背景
 	// 导航栏
@@ -40,21 +70,104 @@ void HariMain(void)
 	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);  //  底部导航栏
 	// 按钮
 	boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24); // 白色margin上
-	boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4); // 白色margin左
-	boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4); // 灰色margin下
+	boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);   // 白色margin左
+	boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);   // 灰色margin下
 	boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5); // 灰色margin右
-	boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3); // 黑色margin下
+	boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);   // 黑色margin下
 	boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3); // 黑色margin右
 	// 框
 	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24); // 灰色padding上
 	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4); // 灰色padding左
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3); // 白色padding下
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3); // 白色padding右
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);   // 白色padding下
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);   // 白色padding右
+}
 
-	for (;;)
-	{
-		io_hlt();
+void init_mouse_cursor8(char *mouse, char bc)
+/* 准备鼠标指针16x16 */
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
 	}
+	return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, 
+	int px0, int py0, char *buf, int bxsize)
+{
+	int x,y;
+	for(y=0;y<pysize;y++){
+		for(x=0;x<pxsize;x++){
+			vram[(py0+y)*vxsize+(px0+x)]=buf[y*bxsize+x];
+		}
+	}
+}
+
+void putfont8_asc(unsigned char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for (; *s != 0x00; s++)
+	{
+		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+		x += 8;
+	}
+	return;
+}
+void putfont8(unsigned char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char d, *p;
+	for (i = 0; i < 16; i++)
+	{
+		d = font[i];
+		p = vram + (y + i) * xsize + x;
+		if ((d & 0x80) != 0)
+			p[0] = c;
+		if ((d & 0x40) != 0)
+			p[1] = c;
+		if ((d & 0x20) != 0)
+			p[2] = c;
+		if ((d & 0x10) != 0)
+			p[3] = c;
+		if ((d & 0x08) != 0)
+			p[4] = c;
+		if ((d & 0x04) != 0)
+			p[5] = c;
+		if ((d & 0x02) != 0)
+			p[6] = c;
+		if ((d & 0x01) != 0)
+			p[7] = c;
+	}
+	return;
 }
 
 void init_palette(void)
